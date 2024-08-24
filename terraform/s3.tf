d2f3efcfcbae859c0e2d32d73113944c3544d2f3
Raymond <debillion13@gmail.com>
2024-08-24 18:03:35 +0300

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,13 @@
 # TODO create an S3 bucket.
-
+module "s3_bucket" {
+  source           = "./modules/terraform-aws-s3-bucket"
+  bucket           = "var.bucket"
+  enable_versioning = true
+  tags = {
+    Environment = "dev"
+    Owner       = "user"
+  }
+}
 #resource "aws_s3_bucket_object" "textfile" {
 #  bucket                 = local.files_bucket
 #  key                    = "textfile.txt"
