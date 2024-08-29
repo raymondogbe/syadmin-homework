0d88a19935e9763f1e0d86f1c8a338fc81bc17a4
Raymond <debillion13@gmail.com>
2024-08-29 16:29:28 +0300

    Update install-awscli.sh

--- tfrunner/filesystem/mnt/install-awscli.sh
+++ tfrunner/filesystem/mnt/install-awscli.sh
@@ -1,6 +1,7 @@
 echo "Install AWS cli"
 
 curl -o "/tmp/awscliv2.zip" "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+curl -o "/tmp/awscliv2.zip" "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip"
 unzip /tmp/awscliv2.zip -d /tmp/awscliv2
 
 bash /tmp/awscliv2/aws/install
